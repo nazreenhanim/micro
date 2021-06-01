f0fd299b9d00694cb15f6ced69d5e8c3d363bfbb
Nur Nazreen Hanim binti Japlus <nazreen.hanim@celcom.com.my>
2021-06-02 03:10:32 +0800

    Fixed some styling

--- src/assets/js/on-hover.js
+++ src/assets/js/on-hover.js
@@ -1,25 +1,34 @@
 //Play an animation back on second click
 
-let iconMenu = document.querySelector('.bodymovinanim');
+// let iconMenu = document.querySelector('.bodymovinanim');
 
-    let animationMenu = bodymovin.loadAnimation({
-            container: iconMenu,
-            renderer: 'svg',
-            loop: false,
-            autoplay: false,
-            path: "https://assets9.lottiefiles.com/packages/lf20_Zz37yH.json"
-    });
+// let animationMenu = bodymovin.loadAnimation({
+//         container: iconMenu,
+//         renderer: 'svg',
+//         loop: false,
+//         autoplay: false,
+//         path: "./assets/json/grumpy-cat.json"
+// });
 
-    var directionMenu = 1;
-      iconMenu.addEventListener('mouseenter', (e) => {
-      animationMenu.setDirection(directionMenu);
-      animationMenu.play();
-    });
+// var directionMenu = 1;
+//     iconMenu.addEventListener('mouseenter', (e) => {
+//     animationMenu.setDirection(directionMenu);
+//     animationMenu.play();
+// });
 
-      iconMenu.addEventListener('mouseleave', (e) => {
-      animationMenu.setDirection(-directionMenu);
-      animationMenu.play();
-    });
+//     iconMenu.addEventListener('mouseleave', (e) => {
+//     animationMenu.setDirection(-directionMenu);
+//     animationMenu.play();
+// });
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('anim'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: "https://assets9.lottiefiles.com/packages/lf20_Zz37yH.json"
+})
+
 
 
 

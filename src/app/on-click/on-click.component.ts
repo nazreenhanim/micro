8ab0a06462de5c1c617e8117eb59988867402a3e
Nur Nazreen Hanim binti Japlus <nazreen.hanim@celcom.com.my>
2021-06-02 06:05:07 +0800

    On-click animation

--- src/app/on-click/on-click.component.ts
+++ src/app/on-click/on-click.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, NgZone } from '@angular/core';
 import { AnimationOptions } from 'ngx-lottie';
+import { AnimationItem } from 'lottie-web';
 @Component({
   selector: 'app-on-click',
   templateUrl: './on-click.component.html',
@@ -8,12 +9,30 @@ import { AnimationOptions } from 'ngx-lottie';
 })
 export class OnClickComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit(): void {
   }
 
   options: AnimationOptions = {
     path: './assets/json/wobble.json', 
   };
+
+  private animationItem: AnimationItem;
+
+  constructor(private ngZone: NgZone) {}
+
+  animationCreated(animationItem: AnimationItem): void {
+    this.animationItem = animationItem;
+  }
+
+  pause(): void {
+    this.ngZone.runOutsideAngular(() => {
+      this.animationItem.pause();
+    });
+  }
+
+  play(): void {
+    this.ngZone.runOutsideAngular(() => {
+      this.animationItem.play();
+    });
+  }
 }

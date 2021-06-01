a9bfc8be81e3c56d219f7b9dcd85cae8d26e4637
Nur Nazreen Hanim binti Japlus <nazreen.hanim@celcom.com.my>
2021-06-01 13:20:32 +0800

    Updated global css

--- src/app/loader/loader.component.ts
+++ src/app/loader/loader.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-loader',
   templateUrl: './loader.component.html',
-  styleUrls: ['./loader.component.css']
+  styleUrls: ['./loader.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class LoaderComponent implements OnInit {
 
--- src/app/on-scroll/on-scroll.component.ts
+++ src/app/on-scroll/on-scroll.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-on-scroll',
   templateUrl: './on-scroll.component.html',
-  styleUrls: ['./on-scroll.component.css']
+  styleUrls: ['./on-scroll.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class OnScrollComponent implements OnInit {
 

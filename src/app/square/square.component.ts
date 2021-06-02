3f24802b112825a0820dd6970282a80b0b36098d
Nur Nazreen Hanim binti Japlus <nazreen.hanim@celcom.com.my>
2021-06-03 06:49:31 +0800

    Game styling

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -4,10 +4,10 @@ import { Component, Input } from '@angular/core';
   selector: 'app-square',
   template: `
     <button nbButton *ngIf="!value">{{ value }}</button>
-    <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
-    <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
+    <button style="background:var(--blue);" nbButton hero *ngIf="value == 'X'">{{ value }}</button>
+    <button style="background:tomato;" nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
   `,
-  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
+  styles: ['button{border:2px solid #fff; border-radius: 7px; width:100%; height:100%; font-size:4em !important; font-family:Raleway; background:#e5e5e5; color:white;}']
 })
 export class SquareComponent  {
 

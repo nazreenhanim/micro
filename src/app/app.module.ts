13d4e5e966df3327f16d4668d06a0af2b83dfddb
Nur Nazreen Hanim binti Japlus <nazreen.hanim@celcom.com.my>
2021-06-02 22:33:47 +0800

    Remove lottie-interactive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { LottieModule } from 'ngx-lottie';
-import { Lottie } from 'lottie-interactive';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
